a5cf8b464b842cf623d37972d382cd8d0ac9eee0
CattacoCattaco <98298161+CattacoCattaco@users.noreply.github.com>
2026-09-08 21:33:09 -0700

    Fix Stagnant Nylium Drop
    Now it drops netherrack

--- src/client/java/pookstir/pookstirsenhancednether/client/datagen/PookstirsEnhancedNetherBlockLootTableProvider.java
+++ src/client/java/pookstir/pookstirsenhancednether/client/datagen/PookstirsEnhancedNetherBlockLootTableProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.core.HolderLookup;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootSubProvider;
+import net.minecraft.world.level.block.Blocks;
 import pookstir.pookstirsenhancednether.PookstirsEnhancedNetherBlocks;
 
 public class PookstirsEnhancedNetherBlockLootTableProvider extends FabricBlockLootSubProvider {
@@ -18,6 +19,6 @@ public class PookstirsEnhancedNetherBlockLootTableProvider extends FabricBlockLo
     public void generate() {
         dropSelf(PookstirsEnhancedNetherBlocks.STAGNANT_WART_BLOCK);
         dropSelf(PookstirsEnhancedNetherBlocks.STAGNANT_STEM);
-        dropSelf(PookstirsEnhancedNetherBlocks.STAGNANT_NYLIUM);
+        dropOther(PookstirsEnhancedNetherBlocks.STAGNANT_NYLIUM, Blocks.NETHERRACK.asItem());
     }
 }
